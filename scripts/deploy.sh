6c73c9c28619740d1538d060801e24bb60098f3d
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-01-21 13:08:54 -0500

    Correct directory for deploying site

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rsync --checksum --delete -avz _book/* reclaim:~/public_html/dh-r/ \
+rsync --checksum --delete -avz _book/* \
+  reclaim:~/public_html/dh-r.lincolnmullen.com/ \
 	--exclude Mullen-ComputationalHistoricalThinking.pdf \
 	--exclude Mullen-ComputationalHistoricalThinking.epub \
 	--exclude Mullen-ComputationalHistoricalThinking.mobi
